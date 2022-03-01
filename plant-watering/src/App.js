46bd0cb2ede0677fca5395f454e739d0cd90c7da
mack edwards <mackedwards@macks-MacBook-Air.local>
2022-03-01 14:26:12 -0700

    working front end

--- plant-watering/src/App.js
+++ plant-watering/src/App.js
@@ -3,33 +3,41 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [plants, setPlants] = useState([])
   const [newName, setNewName] = useState('')
-  const [newDescription, setNewDescription] = useState('')
   const [newImage, setNewImage] = useState('')
+  const [newDescription, setNewDescription] = useState('')
+  const [newWatered, setNewWatered] = useState(false)
+  const [plants, setPlants] = useState([])
 
   useEffect(()=>{
-    axios.get('http://localhost:3000/plants').then((response)=>{
-      setPlants(response.data)
+    axios
+      .get('http://localhost:3000/plants')
+      .then((response)=>{
+        setPlants(response.data)
     })
   }, [])
 
-  const formSubmitt = (event) => {
-    axios.post('http://localhost:3000/plants',
-  {
-    name: newName,
-    description: newDescription,
-    image: newImage
-  }).then((response)=>{
-    axios.get('http://localhost:3000/plants').then((response)=>{
-      setPlants(response.data)
+  const handleNewFormSubmit = (event) => {
+    event.preventDefault();
+    axios.post(
+      'http://localhost:3000/plants',
+      {
+        name: newName,
+        image: newImage,
+        description: newDescription,
+        wasWatered: newWatered
+      }).then((response)=>{
+        axios
+          .get('http://localhost:3000/plants')
+          .then((response)=>{
+            setPlants(response.data)
+        })
     })
-  })
   }
 
-  const handleDelete = (plantsData) => {
+  const handleDelete = (plantData) => {
     axios
-      .delete(`http://localhost:3000/plants/${plantsData._id}`)
+      .delete(`http://localhost:3000/plants/${plantData._id}`)
       .then(() => {
           axios
             .get('http://localhost:3000/plants')
@@ -39,35 +47,73 @@ const App = () => {
       })
   }
 
-  const newImageChange = (event) => {
-    setNewImage(event.target.value)
+  const handleToggleWatered = (plantData) => {
+    axios
+      .put(`http://localhost:3000/plants/${plantData._id}`,
+        {
+          name: plantData.name,
+          image: plantData.image,
+          description: plantData.description,
+          wasWatered:!plantData.wasWatered
+        }
+      )
+      .then(() => {
+        axios
+          .get('http://localhost:3000/plants')
+          .then((response) => {
+            setPlants(response.data)
+          })
+      })
   }
-  const newNameChange = (event) => {
+
+  const handleNewNameChange = (event) => {
     setNewName(event.target.value)
   }
-  const newDescriptionChange = (event) => {
+  const handleNewImageChange = (event) => {
+    setNewImage(event.target.value)
+  }
+  const handleNewDescriptionChange = (event) => {
     setNewDescription(event.target.value)
   }
-
-  return(
-    <div>
-    <h1>this works</h1>
-    {
-    plants.map((plants)=>{
-      return <div className='plants' key={plants._id}>
-      <div>
-      <h1>{plants.name}</h1>
-      <p>{plants.description}</p>
-      <img src={plants.image}/>
-      <button onClick={ (event)=>{ handleDelete(plants) } }>Delete Plant</button>
-      </div>
-      </div>
-
-    })
+  const handleNewWateredChange = (event) => {
+    setNewWatered(event.target.checked)
   }
 
-    </div>
-  )
-}
+  return (
+    <div>
+    <h1>Plants List</h1>
+    <section>
+      <h2>Add a New Plant</h2>
+      <form onSubmit={handleNewFormSubmit}>
+        Name: <input type="text" onChange={handleNewNameChange}/><br/>
+        Image: <input type="text" onChange={handleNewImageChange}/><br/>
+        Description: <input type="text" onChange={handleNewDescriptionChange}/><br/>
+        Watered: <input type="checkbox" onChange={handleNewWateredChange}/><br/>
+        <input type="submit" value="Add Plant"/>
+      </form>
+    </section>
+    <section>
+    <h2>Plants!</h2>
+      {
+        plants.map((plant)=>{
+          return <div key={plant._id}>
+            <h1>{plant.name}</h1>
+            <p>{plant.description}</p>
+            <p onClick={ (event)=>{ handleToggleWatered(plant) }}>
+            {
+              (plant.wasWatered)?
+                'needs water'
+                :
+                'does not need water'
+            } <br/>
+            </p>
+            <img src={plant.image}/><br/>
+            <button onClick={ (event)=>{ handleDelete(plant) } }>Delete Plant</button>
+          </div>
+        })
+      }
+    </section>
+  </div>
+)}
 
-export default App;
+export default App
