413994cfb0506c4bb53c748dd2289e1acbb0ca75
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-03-01 15:00:59 -0500

    half of front end done

--- plant-watering/src/App.js
+++ plant-watering/src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [plants, setPlants] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newDescription, setNewDescription] = useState('')
+  const [newImage, setNewImage] = useState('')
+
+  const newImageChange = (event) => {
+    setNewImage(event.target.value)
+  }
+  const newNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const newDescriptionChange = (event) => {
+    setNewDescription(event.target.value)
+  }
+  const formSubmitt = (event) => {
+    axios.post('http://localhost:3000/plants',
+  {
+    name: newName,
+    description: newDescription,
+    image: newImage
+  }).then((resposne)=>{
+    axios.get('http://localhost:3000/plants').then((response)=>{
+      setPlants(response.data)
+    })
+  })
+  }
+  useEffect(()=>{
+    axios.get('http://localhost:3000/plants').then((response)=>{
+      setPlants(response.data)
+    })
+  }, [])
+
+  return(
+    <div>
+    <h1>this works</h1>
+    {
+    plants.map((plants)=>{
+      return <div className='plants' key={plants._id}>
+      <div>
+      <h1>{plants.name}</h1>
+      <p>{plants.description}</p>
+      <img src={plants.image}/>
+      </div>
+      </div>
+
+    })
+  }
+  
     </div>
-  );
+  )
 }
 
 export default App;
