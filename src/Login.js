f32a53787421a7c3985c39eb0f9b3ff42f9e6aba
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-03-04 13:42:11 -0500

    got login to work

--- src/Login.js
+++ src/Login.js
@@ -35,8 +35,8 @@ const Login = () => {
       )
       console.log(JSON.stringify(response?.data));
         // console.log(JSON.stringify(response));
-      const roles = response?.data?.roles
-      setAuth({ user, pwd})
+
+    
       setUser()
       setPwd()
       setSuccess(true)
